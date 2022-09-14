8f2de42c40200f7a944d0e0c2cf43dfd3d14bb39
Sourav Paul <111589971+Sourav162306@users.noreply.github.com>
2022-09-14 14:49:07 +0530

    Update HistoryEl.js

--- src/Components/History/HistoryEl.js
+++ src/Components/History/HistoryEl.js
@@ -1,25 +1,38 @@
-import './HistoryEl.css'
+import './history.css'
 
 const HistoryEl = props => {
-  const {historyDetails} = props
-  const {timeAccessed, logoUrl, title, domainUrl} = historyDetails
+  const {historyDetails, deleteItem} = props
+  const {id, timeAccessed, logoUrl, title, domainUrl} = historyDetails
+
+  const onDelete = () => {
+    deleteItem(id)
+  }
+
   return (
     <li className="list-item">
       <div className="first-part">
         <p className="time">{timeAccessed}</p>
         <div className="logo-container">
-          <img src={logoUrl} alt={title} className="app-logo-img" />
+          <img src={logoUrl} alt="domain logo" className="app-logo-img" />
           <p className="title">{title}</p>
-          <p className="domain-url">{domainUrl}</p>
+          <a
+            href={domainUrl}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="domain-url"
+          >
+            {domainUrl}
+          </a>
         </div>
       </div>
-      <div className="second-part">
+      <button type="button" className="second-part">
         <img
           src="https://assets.ccbp.in/frontend/react-js/delete-img.png"
           alt="delete"
           className="logo-delete"
+          onClick={onDelete}
         />
-      </div>
+      </button>
     </li>
   )
 }
